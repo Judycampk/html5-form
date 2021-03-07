9a0f5673745cd8c0365ceb12b628bb6c89ce6875
Judycampk <judhycampk71@outlook.com>
2021-03-07 01:05:59 +0000

    Validacion Terminada

--- validation.js
+++ validation.js
@@ -1,12 +1,8 @@
-// Selecciona todo formulario
 const submitForm = document.getElementById("form")
 
 submitForm.addEventListener("submit", function (event) {
-
-    // Previene enviar el formulario sin hacer validacion
     event.preventDefault()
 
-    // Inputs
     let card = document.getElementById("card");
     let cvc = document.getElementById("cvc");
     let amount = document.getElementById("amount");
@@ -16,8 +12,6 @@ submitForm.addEventListener("submit", function (event) {
     let state = document.getElementById("stateInput");
     let postal = document.getElementById("postalCode");
 
-    // Credit Card
-    // Regular expression for credit cards
     const creditPattern = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/
     let result = creditPattern.test(card.value)
     if (!result) {
